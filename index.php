b2cea2be99fcadfd9fe313b2478e55e5b88ee2bf
Leonardo Capogna <leonardocapogna@gmail.com>
2021-01-25 10:54:30 +0100

    style da collegare

--- index.php
+++ index.php
@@ -1,34 +1,111 @@
 <?php
-// Stampare a schermo una decina di dischi musicali
-// (come nello screenshot presente a fine slide 41).
-// Prima Milestone:
-// Stampiamo i dischi solo con l’utilizzo di PHP:
-// al caricamento della pagina, ci saranno tutti i dischi.
-// Utilizzare:
-// Html, Sass, PHP
-// Consiglio: come primo step installate
-// laravel-mix nella cartella e provate SASS.
-
-  include __DIR__ . "/db.php";
-  include_once __DIR__ . '/functions.php';
+$discs = [
+  [
+    "poster" => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+    "title"=> "New Jersey",
+    "author"=> "Bon Jovi",
+    "genre"=> "Rock",
+    "year"=> "1988"
+  ],
+  [
+    "poster" => "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
+    "title"=> "Live at Wembley 86",
+    "author"=> "Queen",
+    "genre"=> "Pop",
+    "year"=> "1992"
+  ],
+  [
+    "poster" => "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
+    "title"=> "Ten's Summoner's Tales",
+    "author"=> "Sting",
+    "genre"=> "Pop",
+    "year"=> "1993"
+  ],
+  [
+    "poster" => "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+    "title"=> "Steve Gadd Band",
+    "author"=> "Steve Gadd Band",
+    "genre"=> "Jazz",
+    "year"=> "2018"
+  ],
+  [
+    "poster" => "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
+    "title"=> "Brave new World",
+    "author"=> "Iron Maiden",
+    "genre"=> "Metal",
+    "year"=> "2000"
+  ],
+  [
+    "poster" => "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+    "title"=> "One more car, one more raider",
+    "author"=> "Eric Clapton",
+    "genre"=> "Rock",
+    "year"=> "2002"
+  ],
+  [
+    "poster" => "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+    "title"=> "Made in Japan",
+    "author"=> "Deep Purple",
+    "genre"=> "Rock",
+    "year"=> "1972"
+  ],
+  [
+    "poster" => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+    "title"=> "And Justice for All",
+    "author"=> "Metallica",
+    "genre"=> "Metal",
+    "year"=> "1988"
+  ],
+  [
+    "poster" => "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+    "title"=> "Hard Wired",
+    "author"=> "Dave Weckl",
+    "genre"=> "Jazz",
+    "year"=> "1994"
+  ],
+  [
+    "poster" => "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
+    "title"=> "Bad",
+    "author"=> "Michael Jackson",
+    "genre"=> "Pop",
+    "year"=> "1987"
+  ],
+];
 ?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC&family=Roboto&family=Yusei+Magic&display=swap" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <!-- css -->
     <link rel="stylesheet" href="css/style.css">
+    <meta charset="utf-8">
     <title>Dischi</title>
   </head>
   <body>
-    <?php include __DIR__ . '/partials/header.php'; ?>
 
-    <div class="container">
+    <header>
+      <i class="fab fa-spotify"></i>
+    </header>
 
-    </div>
+    <main>
+      <div class="container">
+        <div class="disc-box">
+          <?php foreach ($discs as $cd) { ?>
+            <div class="cover">
+              <img src="<?= $cd["poster"]; ?>" alt="<?= $cd["title"]; ?>">
+              <h3><?= $cd["title"]; ?></h3>
+              <small><?= $cd["author"]; ?></small>
+              <strong><?= $cd["year"]; ?></strong>
+            </div>
+          <?php } ?>
+        </div>
+      </div>
 
-    <?php include __DIR__ . '/partials/footer.php'; ?>
+    </main>
   </body>
 </html>
